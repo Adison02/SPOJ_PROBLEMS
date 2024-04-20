3a0eed20552bcf9aeae3b3bd977380790dce7f85
Adrian Mazur <adrianmazur0660@gmail.com>
2024-04-20 12:29:38 +0200

    2 dodatkowe zadania

---
+++ SPOJ_PROBLEMS/Koncert.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// WSEIHACK2004A - Koncert(Code golf) (684 znaki)
+/// </summary>
+public static class Koncert
+{
+    public static void Run()
+    {
+        var t = int.Parse(Console.ReadLine());
+        while (t-- > 0)
+        {
+            var input = Console.ReadLine().Split(',');
+            var sum = 0;
+            for (int i = 0; i < input.Count(); i++)
+            {
+                var time = input[i].Split(':');
+                var minutes = int.Parse(time[0]);
+                var seconds = int.Parse(time[1]);
+                sum += minutes * 60 + seconds;
+            }
+            var result = (int)Math.Ceiling((double)sum / input.Count());
+            Console.Write(input.Count() + " " + format(result) + " " + format(sum) + "\n");
+        }
+
+        string format(int sum)
+        {
+            var hour = sum / 3600;
+            if (hour == 0)
+                return $"{(sum / 60) % 60}:{sum % 60:00}";
+            else
+                return $"{hour}:{(sum / 60) % 60:00}:{sum % 60:00}";
+        }
+    }
+}
---
+++ SPOJ_PROBLEMS/STPAR.cs
@@ -0,0 +1,44 @@
+﻿/// <summary>
+/// STPAR - Street Parade
+/// </summary>
+public static class STPAR
+{
+    public static void Run()
+    {
+        int n = int.Parse(Console.ReadLine());
+        do
+        {
+            int[] mobiles = Console.ReadLine().Split().Select(mobile => int.Parse(mobile)).ToArray<int>();
+
+            Stack<int> stack = new Stack<int>();
+            int need = 1;
+            bool poss = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                while (stack.Any() && stack.Peek() == need)
+                {
+                    need++;
+                    stack.Pop();
+                }
+
+                if (mobiles[i] == need)
+                {
+                    need++;
+                } else if (stack.Any() && stack.Peek() < mobiles[i])
+                {
+                    poss = false;
+                    break;
+                } else
+                {
+                    stack.Push(mobiles[i]);
+                }
+
+            }
+
+            Console.WriteLine(poss ? "yes" : "no");
+            n = int.Parse(Console.ReadLine());
+        }
+        while (n != 0);
+    }
+}
